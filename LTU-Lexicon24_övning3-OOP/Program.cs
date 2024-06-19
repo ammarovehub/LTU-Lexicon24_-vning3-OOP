b1a06d82b4a739225c19fc390923770b53c955df
ammarovehub <ammarove1@pm.me>
2024-06-19 20:11:27 +0200

    Used the personhandler to create multiple "person" objects and used the "set" methods to modify the  attributes of these "person" objects.
    Finally I added print that I forgot from the beganing to verfiy the changes went well.

--- LTU-Lexicon24_övning3-OOP/Program.cs
+++ LTU-Lexicon24_övning3-OOP/Program.cs
@@ -7,14 +7,34 @@ using Encapsulation;
         static void Main(string[] args)
         {
             PersonHandler handler = new PersonHandler();
+            Person person1 = handler.CreatePerson(35, "Olivia", "Johanson", 1.65, 60);
+            Person person2 = handler.CreatePerson(65, "Elias", "Sjölund", 1.70, 85);
+
             try
             {
-                Person person = handler.CreatePerson(30, "Ammar", "Jamous", 1.67, 70);
-                handler.SetAge(person, 10);
+                handler.SetAge(person1, 10);
             }
             catch (ArgumentException ex)
             {
-                Console.WriteLine(ex.Message);
+                Console.WriteLine($"Error updating person1: {ex.Message}");
             }
+
+            try 
+            {
+                handler.SetWeight(person2, 60);
+                handler.SetHeight(person2, 1.80);
+            }
+            catch (ArgumentException ex) 
+            {
+                Console.WriteLine($"Error updating person2: {ex.Message}");
+            }
+
+            PrintPersonDetails(person1);
+            PrintPersonDetails(person2);
+        }
+        static void PrintPersonDetails(Person person)
+        {
+            Console.WriteLine($"Person Details: Age={person.Age}, FirstName={person.FName}, LastName={person.LName}, Height={person.Height}, Weight={person.Weight}");
         }
     }
+        
