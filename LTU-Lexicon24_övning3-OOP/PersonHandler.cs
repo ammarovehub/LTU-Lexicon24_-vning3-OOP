257de0c61ffce397beacfd5e1b3927664cab1de5
ammarovehub <ammarove1@pm.me>
2024-06-19 18:45:38 +0200

    Simplify personhandler.cs

--- LTU-Lexicon24_övning3-OOP/PersonHandler.cs
+++ LTU-Lexicon24_övning3-OOP/PersonHandler.cs
@@ -2,6 +2,10 @@
 {
     public class PersonHandler
     {
+        public Person CreatePerson(int age, string fname, string lname, double height, double weight)
+        {
+            return new Person(age, fname, lname, height, weight);
+        }
         public void SetAge(Person pers, int age) 
         {
             pers.Age = age;
@@ -12,22 +16,9 @@
             pers.Height = height;
         }
 
-        public void SetWeight(Person pers, int weight) 
+        public void SetWeight(Person pers, int weight)
         {
             pers.Weight = weight;
-        }
-
-        public Person CreatePerson(int age, string fname, string lname, double height, double weight)
-        {
-            return new Person
-            {
-                Age = age,
-                FName = fname,
-                LName = lname,
-                Height = height,
-                Weight = weight
-            };
-            
         }
     }
 }
